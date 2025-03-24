387a17a4983ccdb124111b29ff560b768fa172c7
Jitin Rathi <jitinrathi910949@gmail.com>
2025-03-24 16:10:36 +0530

    new udpates

--- src/api-mocks/handlers/people.handler.ts
+++ src/api-mocks/handlers/people.handler.ts
@@ -1,4 +1,4 @@
-import { rest } from "msw";
+import { http, delay, HttpResponse } from 'msw';
 
 import { API_RESOURCE } from "../../app/shared/constant";
 import { PEOPLE } from "../fixtures";
@@ -6,8 +6,9 @@ import { delayedResponse } from "../utils";
 
 const BASE_URL = `/mock-api/${API_RESOURCE.PEOPLE}*`;
 
-export const getPeople = rest.get(BASE_URL, (_req, _res, ctx) =>
-  delayedResponse(ctx.status(200), ctx.json(PEOPLE))
-);
+export const getPeople = http.get(BASE_URL, () => {
+  delay(500);
+  return HttpResponse.json(PEOPLE);
+});
 
 export const handlers = [getPeople];
